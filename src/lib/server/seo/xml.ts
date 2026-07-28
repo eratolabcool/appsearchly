05e0aaf9c5e40cdd1387a7658d7bf297179e5a45
eratolabcool <eratolabcool@gmail.com>
2026-07-28 09:01:51 +0800

    feat(seo): add sitemap XML helpers

---
+++ src/lib/server/seo/xml.ts
@@ -0,0 +1,20 @@
+export const SITE_URL = 'https://appsearchly.com';
+export const TOOL_SITEMAP_PAGE_SIZE = 1000;
+
+export function escapeXml(value: string): string {
+  return value
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&apos;');
+}
+
+export function xmlResponse(body: string): Response {
+  return new Response(body, {
+    headers: {
+      'content-type': 'application/xml; charset=utf-8',
+      'cache-control': 'public, max-age=300, s-maxage=3600'
+    }
+  });
+}
